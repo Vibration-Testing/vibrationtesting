cfe0b6d991517cc69743e05cffcecbfcb3f96fd1
Joseph C. Slater <joseph.c.slater@gmail.com>
2017-11-23 16:46:18 -0500

    rounding...

--- vibrationtesting/sigp.py
+++ vibrationtesting/sigp.py
@@ -1369,7 +1369,7 @@ def so2ss(M, C, K, Bt, Cd, Cv, Ca):
     >>> Cd = Cv = np.zeros((1,2))
     >>> Ca = np.array([[1, 0]])
     >>> A, B, Css, D = vt.so2ss(M, C, K, Bt, Cd, Cv, Ca)
-    >>> np.set_printoptions(precision=4)
+    >>> np.set_printoptions(precision=4, suppress = True)
     >>> print('A: {}'.format(A))
     A:  [[ 0.      0.      1.      0.    ]
      [ 0.      0.      0.      1.    ]
