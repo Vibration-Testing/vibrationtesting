fbfe34eb12a718d481087f00d99bca2325fe57c3
Joseph C. Slater <joseph.c.slater@gmail.com>
2017-11-25 14:31:55 -0500

    docstrings working and all pretty

--- vibrationtesting/system.py
+++ vibrationtesting/system.py
@@ -12,14 +12,55 @@ import warnings
 
 import numpy as np
 # import control as ctrl
-import scipy.signal as sig
+# import scipy.signal as sig
 import scipy.linalg as la
 
 
 def d2c(Ad, Bd, C, D, dt):
-    """Returns continuous A, B, C, D from discrete Ad, Bd, C, D
+    """Returns continuous A, B, C, D from discrete
     Converts a set of digital state space system matrices to their
     continuous counterpart.
+    Parameters
+    ----------
+    Ad, Bd, C, D : float arrays
+        Discrete state space system matrices
+    dt  : float
+        Time step of discrete system
+
+    Returns
+    -------
+    A, B, C, D : float arrays
+        State space system matrices
+
+    Examples
+    --------
+    >>> import vibrationtesting as vt
+    >>> A1 = np.array([[ 0.9999,0.0001,0.01,0.]])
+    >>> A2 = np.array([[ 0.,0.9999,0.,0.01]])
+    >>> A3 = np.array([[-0.014,0.012,0.9999,0.0001]])
+    >>> A4 = np.array([[ 0.008, -0.014,0.0001,0.9999]])
+    >>> A = np.concatenate((A1, A2, A3, A4))
+    >>> print(A)
+    [[ 0.9999  0.0001  0.01    0.    ]
+     [ 0.      0.9999  0.      0.01  ]
+     [-0.014   0.012   0.9999  0.0001]
+     [ 0.008  -0.014   0.0001  0.9999]]
+    >>> B = np.array([[ 0.  ], [ 0.  ], [ 0.  ], [ 0.01]])
+    >>> C = np.array([[-1.4, 1.2, -0.0058, 0.0014]])
+    >>> D = np.array([[-0.2]])
+    >>> A, B, *_ = vt.d2c(A, B, C, D, 0.01)
+    >>> print(A)
+    [[-0.003   0.004   1.0001 -0.0001]
+     [-0.004  -0.003  -0.      1.0001]
+     [-1.4001  1.2001 -0.003   0.004 ]
+     [ 0.8001 -1.4001  0.006  -0.003 ]]
+
+    Notes
+    -----
+    .. note:: Zero-order hold solution
+    .. note:: discrepancies between :func:`c2d` and :func:`d2c` are due to \
+    typing truncation errors.
+    .. seealso:: :func:`c2d`.
     """
     A = la.logm(Ad) / dt
     B = la.solve((Ad - np.eye(A.shape[0])), A) @ Bd
@@ -27,44 +68,104 @@ def d2c(Ad, Bd, C, D, dt):
 
 
 def c2d(A, B, C, D, dt):
-    """Returns discrete Ad, Bd, C, D from continuous A, B, C, D
+    """Convert continuous state system to discrete time
     Converts a set of continuous state space system matrices to their
     discrete counterpart.
 
     Parameters
     ----------
-
+    A, B, C, D : float arrays
+        State space system matrices
+    dt : float
+        Time step of discrete system
 
     Returns
     -------
+    Ad, Bd, C, D : float arrays
+        Discrete state space system matrices
 
     Examples
     --------
     >>> import numpy as np
     >>> import vibrationtesting as vt
-    >>> M = np.array([[2, 1],[1, 3]])
-    >>> K = np.array([[2, -1],[-1, 3]])
-    >>> C = np.array([[0.01, 0.001],[0.001, 0.01]])
-    >>> Bt = np.array([[0],[1]])
-    >>> Cd = Cv = np.zeros((1,2))
-    >>> Ca = np.array([[1, 0]])
-    >>> A, B, Css, D = vt.so2ss(M, C, K, Bt, Cd, Cv, Ca)
+    >>> A1 = np.array([[ 0.,   0. ,  1.    ,  0.    ]])
+    >>> A2 = np.array([[ 0.,   0. ,  0.    ,  1.    ]])
+    >>> A3 = np.array([[-1.4,  1.2, -0.0058,  0.0014]])
+    >>> A4 = np.array([[ 0.8, -1.4,  0.0016, -0.0038]])
+    >>> A = np.concatenate((A1, A2, A3, A4))
+    >>> print(A)
+    [[ 0.      0.      1.      0.    ]
+     [ 0.      0.      0.      1.    ]
+     [-1.4     1.2    -0.0058  0.0014]
+     [ 0.8    -1.4     0.0016 -0.0038]]
+    >>> B = np.array([[ 0.], [ 0.], [ 0.], [ 1.]])
+    >>> C = np.array([[-1.4, 1.2, -0.0058, 0.0014]])
+    >>> D = np.array([[-0.2]])
+    >>> Ad, Bd, *_ = vt.c2d(A, B, C, D, 0.01)
+    >>> print(Ad)
+    [[ 0.9999  0.0001  0.01    0.    ]
+     [ 0.      0.9999  0.      0.01  ]
+     [-0.014   0.012   0.9999  0.0001]
+     [ 0.008  -0.014   0.0001  0.9999]]
+    >>> print(Bd)
+    [[ 0.  ]
+     [ 0.  ]
+     [ 0.  ]
+     [ 0.01]]
 
-    Converts a set of digital state space system matrices to their
-    continuous counterpart.
-    Simply calls scipy.signal.cont2discrete
+    Notes
+    -----
+    .. note:: Zero-order hold solution
+    .. seealso:: :func:`d2c`.
     """
 
-    Ad, Bd, _, _, _ = sig.cont2discrete((A, B, C, D), dt)
+    # Ad, Bd, _, _, _ = sig.cont2discrete((A, B, C, D), dt)
     Ad = la.expm(A * dt)
     Bd = la.solve(A, (Ad - np.eye(A.shape[0]))) @ B
     return Ad, Bd, C, D
 
 
 def ssfrf(A, B, C, D, omega_low, omega_high, in_index, out_index):
-    """returns omega, H
+    """returns
     obtains the computed FRF of a state space system between selected input
     and output over frequency range of interest.
+
+    Parameters
+    ----------
+    A, B, C, D : float arrays
+                 state system matrices
+    omega_low, omega_high: floats
+                 low and high frequencies for evaluation
+    in_index, out_index : ints
+                input and output numbers (starting at 1)
+
+    Returns
+    -------
+    omega : float array
+            frequency vector
+    H : float array
+        frequency response function
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> import vibrationtesting as vt
+    >>> A1 = np.array([[ 0.,   0. ,  1.    ,  0.    ]])
+    >>> A2 = np.array([[ 0.,   0. ,  0.    ,  1.    ]])
+    >>> A3 = np.array([[-1.4,  1.2, -0.0058,  0.0014]])
+    >>> A4 = np.array([[ 0.8, -1.4,  0.0016, -0.0038]])
+    >>> A = np.concatenate((A1, A2, A3, A4))
+    >>> print(A)
+    [[ 0.      0.      1.      0.    ]
+     [ 0.      0.      0.      1.    ]
+     [-1.4     1.2    -0.0058  0.0014]
+     [ 0.8    -1.4     0.0016 -0.0038]]
+    >>> B = np.array([[ 0.], [ 0.], [ 0.], [ 1.]])
+    >>> C = np.array([[-1.4, 1.2, -0.0058, 0.0014]])
+    >>> D = np.array([[-0.2]])
+    >>> omega, H = vt.ssfrf(A, B, C, D, 0, 3.5, 1, 1)
+    >>> vt.frfplot(omega, H)
+
     """
     # A, B, C, D = ctrl.ssdata(sys)
     sa = A.shape[0]
@@ -72,9 +173,9 @@ def ssfrf(A, B, C, D, omega_low, omega_high, in_index, out_index):
     H = omega * 1j
     i = 0
     for i, w in enumerate(omega):
-        H[i] = (C@la.solve(w * 1j * np.eye(sa) - A, B) + D)[out_index,
-                                                            in_index]
-    return omega, H
+        H[i] = (C@la.solve(w * 1j * np.eye(sa) - A, B) + D)[out_index-1,
+                                                            in_index-1]
+    return omega.reshape(1, -1), H.reshape(1, -1)
 
 
 def so2ss(M, C, K, Bt, Cd, Cv, Ca):
@@ -119,14 +220,13 @@ def so2ss(M, C, K, Bt, Cd, Cv, Ca):
     --------
     >>> import numpy as np
     >>> import vibrationtesting as vt
-    >>> M = np.array([[2, 1],[1, 3]])
-    >>> K = np.array([[2, -1],[-1, 3]])
-    >>> C = np.array([[0.01, 0.001],[0.001, 0.01]])
-    >>> Bt = np.array([[0],[1]])
+    >>> M = np.array([[2, 1], [1, 3]])
+    >>> K = np.array([[2, -1], [-1, 3]])
+    >>> C = np.array([[0.01, 0.001], [0.001, 0.01]])
+    >>> Bt = np.array([[0], [1]])
     >>> Cd = Cv = np.zeros((1,2))
     >>> Ca = np.array([[1, 0]])
     >>> A, B, Css, D = vt.so2ss(M, C, K, Bt, Cd, Cv, Ca)
-    >>> np.set_printoptions(precision=4, suppress = True)
     >>> print('A: {}'.format(A))
     A:  [[ 0.      0.      1.      0.    ]
      [ 0.      0.      0.      1.    ]
@@ -143,7 +243,7 @@ def so2ss(M, C, K, Bt, Cd, Cv, Ca):
     D:  [[-0.2]]
     """
 
-    A = np.vstack((np.hstack((np.eye(2) * 0, np.eye(2))),
+    A = np.vstack((np.hstack((np.zeros_like(M), np.eye(M.shape[0]))),
                    np.hstack((-la.solve(M, K), -la.solve(M, C)))))
     B = np.vstack((np.zeros((Bt.shape[0], 1)), Bt))
     C_ss = np.hstack((Cd - Ca@la.solve(M, K), Cv - Ca@la.solve(M, C)))
