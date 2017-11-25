1287ca2fd32fd3e6092d09314be9d0b852084dd6
Joseph C. Slater <joseph.c.slater@gmail.com>
2017-11-25 14:31:11 -0500

    cleanup

--- vibrationtesting/__init__.py
+++ vibrationtesting/__init__.py
@@ -54,34 +54,8 @@ if 'pytest' in sys.argv[0]:
 from .signal import *
 from .system import *
 
-"""
-np.set_printoptions(edgeitems=3, infstr='inf', linewidth=75,
-                    nanstr='nan', precision=8, suppress=False,
-                    threshold=1000, formatter=None)
-                    """
-# from ._max_len_seq import max_len_seq
 
-# The spline module (a C extension) provides:
-#     cspline2d, qspline2d, sepfir2d, symiirord1, symiirord2
-# from .spline import *
 
-# from .bsplines import *
-# from .cont2discrete import *
-# from .dltisys import *
-# from .filter_design import *
-# from .fir_filter_design import *
-# from .ltisys import *
-# from .windows import *
-# from .signaltools import *
-# from ._savitzky_golay import savgol_coeffs, savgol_filter
-# from .spectral import *
-# from .wavelets import *
-# from ._peak_finding import *
-
-# __all__ = [s for s in dir() if not s.startswith('_')]
-# from numpy.testing import Tester
-# test = Tester().test
-# bench = Tester().bench
 
 # print options were change inside modules to produce better
 # outputs at examples. Here we set the print options to the
