c18a0e2a020a05163e98ba3162e264b587cdc98d
Joseph C. Slater <joseph.c.slater@gmail.com>
2018-01-03 22:08:33 -0500

    Keep examples in sync

--- vibrationtesting/signals.py
+++ vibrationtesting/signals.py
@@ -113,8 +113,7 @@ def window(x, windowname='hanning', normalize=False):
     Text(0,0.5,'Hanning windowed $x(t)$')
     >>> ax2.set_xlabel('time')
     Text(0.5,0,'time')
-    >>> ax2.set_title('Effect of window. Note the scaling to conserve
-    ...                                                ASD amplitude')
+    >>> ax2.set_title('Effect of window. Note the scaling to conserve ASD amplitude')
     Text(0.5,1,'Effect of window. Note the scaling to conserve ASD amplitude')
     >>> fig.tight_layout()
 
@@ -257,8 +256,7 @@ def hanning(x, normalize=False):
     Text(0,0.5,'Hanning windowed $x(t)$')
     >>> ax2.set_xlabel('time')
     Text(0.5,0,'time')
-    >>> ax2.set_title('Effect of window. Note the scaling to conserve
-    ...                                                ASD amplitude')
+    >>> ax2.set_title('Effect of window. Note the scaling to conserve ASD amplitude')
     Text(0.5,1,'Effect of window. Note the scaling to conserve ASD amplitude')
     >>> fig.tight_layout()
 
